3bbde0a9b6f358eeb6faf9a6fe7bab21e1f31cd0
khoaace <khoaace@gmail.com>
2018-12-05 21:50:36 +0700

    fix modal trip info

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,10 @@ var express = require("express");
 var router = express.Router();
 var tripsModel = require('../models/tripsModel.js');
 var moment = require('moment');
+var tripsController = require("../controllers/tripsController.js");
 
-/* GET home page. */
-router.get("/error", function(req, res, next) {
-  res.render("error", { title: "Error", message: req.flash("info"), user: req.session.user });
-});
+
+
 
 
 router.get("/", function(req, res, next) {
@@ -37,18 +36,10 @@ router.get("/list-trips/:page", function (req, res, next) {
   });
 });
 
-/*-----------------------------------Xác thực tài khoản----------------------------*/
-router.use(function(req, res, next) {
-  // check header or url parameters or post parameters for token
-  var user = req.session.user;
-  //req.body.token || req.query.token || req.headers['x-access-token']
-  // decode token
-  if (user) {
-      next();
-  } else {
-      req.flash('info','Chưa thực hiện đăng nhập.');
-      res.redirect('/error');
-  }
+
+/* GET home page. */
+router.get("/error", function(req, res, next) {
+  res.render("error", { title: "Error", message: req.flash("info"), user: req.session.user });
 });
 
 
