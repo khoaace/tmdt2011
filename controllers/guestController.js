8dc34c7b049d381cf1baaa3611341e4128d52e7f
khoaace <khoaace@gmail.com>
2019-01-06 17:10:07 +0700

    add feature category

--- controllers/guestController.js
+++ controllers/guestController.js
@@ -1,4 +1,10 @@
+//Controller
 var tripsController = require("../controllers/tripsController");
+var userController = require("../controllers/userController");
+//Model
+var tripsModel = require('../models/tripsModel.js');
+var userModel = require('../models/usersModel');
+//Extend
 var moment = require("moment");
 
 /**
@@ -7,12 +13,141 @@ var moment = require("moment");
  * @description :: Server-side logic for managing tripss.
  */
 module.exports = {
-  guest: function(req, res) {
-    tripsModel.find({}, function (err, trips) {
-        if (err)
-          res.render("index", { title: "EC1805 ", user: req.session.user, trips: trips, moment: moment });
-        else
-          res.render("index", { title: "EC1805 ", user: req.session.user, trips: trips, moment: moment });
+  guest: async function (req, res) {
+    let promise = [];
+
+    promise.push(userModel.find({ agency: true }));
+    promise.push(tripsModel.find());
+
+    await Promise.all(promise).then(result => {
+      let listTrip = result[1] || [];
+      let listAgency = result[0] || [];
+
+      res.render("index", { title: "EC1805 ", user: req.session.user, trips: listTrip, listAgency, moment: moment });
+
+    }).catch(err => {
+      throw err;
+    })
+  },
+  listTrips: async function (req, res) {
+    let perPage = 10;
+    let page = req.params.page || 1;
+
+    tripsModel.paginate({}, { offset: (perPage * page) - perPage, limit: perPage }, async function (err, result) {
+      await tripsModel.find({}, function (err, trips) {
+        if (err) return next(err);
+
+        res.render('guest/list-trips', {
+          title: "List Trips",
+          trips: result.docs,
+          current: page,
+          pages: Math.ceil(trips.length / perPage),
+          moment: moment,
+          user: req.session.user
+        });
+
       });
+    });
+  },
+  listTripsAgency: async function (req, res) {
+    let perPage = 10;
+    let page = req.params.page || 1;
+    let id = req.params.id;  
+
+    tripsModel.paginate({ agency: id }, { offset: (perPage * page) - perPage, limit: perPage }, async function (err, result) {
+      await tripsModel.find({ agency: id }, function (err, trips) {
+        if (err) return next(err);
+        res.render('guest/list-trips', {
+          title: "List Trips",
+          trips: result.docs,
+          current: page,
+          pages: Math.ceil(trips.length / perPage),
+          moment: moment,
+          user: req.session.user
+        });
+
+      });
+    });
+  },
+  listTripsType: async function (req, res) {
+    let perPage = 10;
+    let page = req.params.page || 1;
+    let type = req.params.type;
+
+    tripsModel.paginate({ typeOfBus: type }, { offset: (perPage * page) - perPage, limit: perPage }, async function (err, result) {
+      await tripsModel.find({ typeOfBus: type }, function (err, trips) {
+        if (err) return next(err);
+        res.render('guest/list-trips', {
+          title: "List Trips",
+          trips: result.docs,
+          current: page,
+          pages: Math.ceil(trips.length / perPage),
+          moment: moment,
+          user: req.session.user
+        });
+
+      });
+    });
+  },
+  payment: async function (req, res) {
+    if (req.session.booking) {
+
+      const { id, agency } = req.session.booking;
+      let reservations = [];
+      const tempReservations = req.session.booking['currentReservations[]'];
+      if (typeof (tempReservations) === "string")
+        reservations.push(tempReservations);
+      else
+        reservations = tempReservations;
+      //Generator Code ticket
+      let idBooking = Math.random().toString(36).substring(7);
+      let dateBooking = new Date();
+
+      tripsModel.findOne({ _id: id }, async function (err, trip) {
+        if (err)
+          res.redirect('/');
+        if (!trip)
+          res.redirect('/');
+        else
+
+          await user.findOne({ _id: agency }, function (err, agency) {
+            if (err)
+              res.redirect('/');
+            if (!agency)
+              res.redirect('/');
+            else {
+              const total = parseInt(reservations.length) * parseInt(trip.price);
+              console.log(total);
+              res.render("guest/payment",
+                {
+                  title: "EC1805 - Payment ",
+                  user: req.session.user,
+                  idBooking: idBooking,
+                  trip: trip,
+                  reservations: reservations,
+                  dateBooking: dateBooking,
+                  agency: agency,
+                  total: total,
+                  moment: moment
+                });
+            }
+          });
+
+      });
+
+    }
+    else
+      res.redirect('/');
+  },
+  getPayment: async function (req, res) {
+    //Save Booking information
+    req.session.booking = req.body;
+    res.send('ok');
+  },
+  getError: async function (req, res) {
+    res.render("error", { title: "Error", message: req.flash("info"), user: req.session.user });
   }
+
 };
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var tripsModel = require('../models/tripsModel.js');
+var guestController = require('../controllers/guestController');
 var user = require('../models/usersModel');
 var moment = require('moment');
 
@@ -8,95 +9,19 @@ var moment = require('moment');
 
 // View for All memerber 
 
+router.get("/", guestController.guest);
 
-router.get("/", function (req, res, next) {
-  tripsModel.find({}, function (err, trips) {
-    if (err)
-      res.render("index", { title: "EC1805 ", user: req.session.user, trips: trips, moment: moment });
-    else
-      res.render("index", { title: "EC1805 ", user: req.session.user, trips: trips, moment: moment });
-  });
-});
+router.get("/list-trips/:page", guestController.listTrips);
 
-router.get("/list-trips/:page", function (req, res, next) {
-  let perPage = 10;
-  let page = req.params.page || 1;
-  tripsModel.paginate({}, { offset: (perPage * page) - perPage, limit: perPage }, async function (err, result) {
-    await tripsModel.find({}, function (err, trips) {
-      if (err) return next(err);
-      res.render('guest/list-trips', {
-        title: "Express test ",
-        trips: result.docs,
-        current: page,
-        pages: Math.ceil(trips.length / perPage),
-        moment: moment,
-        user: req.session.user
-      })
-    });
-  });
-});
+router.get("/list-trips-agency/:id/:page", guestController.listTripsAgency);
 
+router.get("/list-trips-type/:type/:page", guestController.listTripsType);
 
+router.get("/payment", isLoggedIn, guestController.payment);
 
+router.post("/getPayment", guestController.getPayment);
 
-router.get("/payment", isLoggedIn, function (req, res, next) {
-  if (req.session.booking) {
-    const { id, agency } = req.session.booking;
-    let reservations = [];
-     const tempReservations = req.session.booking['currentReservations[]'];
-    if(typeof(tempReservations) === "string")
-      reservations.push(tempReservations);
-    else
-      reservations = tempReservations;
-    //Generator Code ticket
-    let idBooking = Math.random().toString(36).substring(7);
-    let dateBooking = new Date();
-
-    tripsModel.findOne({ _id: id }, async function (err, trip) {
-      if (err)
-        res.redirect('/');
-      if (!trip)
-        res.redirect('/');
-      else
-        await user.findOne({ _id: agency }, function (err, agency) {
-          if (err)
-            res.redirect('/');
-          if (!agency)
-            res.redirect('/');
-          else {
-            const total = parseInt(reservations.length)*parseInt(trip.price);
-            console.log(total);
-            res.render("guest/payment", 
-            { title: "EC1805 - Payment ", 
-            user: req.session.user, 
-            idBooking: idBooking, 
-            trip: trip, 
-            reservations: reservations, 
-            dateBooking: dateBooking, 
-            agency: agency, 
-            total: total, 
-            moment: moment });
-          }
-        });
-
-    });
-  }
-  else
-    res.redirect('/');
-});
-
-router.post("/getPayment", function (req, res, next) {
-  //Save Booking information
-  req.session.booking = req.body;
-  res.send('ok');
-});
-
-
-
-/* GET home page. */
-router.get("/error", function (req, res, next) {
-  res.render("error", { title: "Error", message: req.flash("info"), user: req.session.user });
-});
+router.get("/error", guestController.getError);
 
 module.exports = router;
 
